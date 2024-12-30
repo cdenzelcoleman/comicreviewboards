a467f7158b3b067840af27be07af6dea89f2e355
cdenzelcoleman <camdcole@gmail.com>
2024-12-30 00:26:16 -0600

    Index and New Comic Route

--- controllers/comics.js
+++ controllers/comics.js
@@ -1,19 +1,41 @@
 const express = require('express');
 const router = express.Router();
 
-// Middleware to protect selected routes
+// Middleware 
 const ensureSignedIn = require('../middleware/ensure-signed-in');
 
 // All routes start with '/comics'
 
 // GET /comics (index functionality) UN-PROTECTED - all users can access
-router.get('/', (req, res) => {
-  res.send('Rejoice - the comics are here!');
+router.get('/', async (req, res) => {
+  try{
+    const comics = await comic.find().populate('owner').exec();
+    res.render('comics/index.ejs', { comics, user: req.user});
+  } catch (err) {
+    console.error(err);
+    res.redirect('/');
+  }
 });
 
 // GET /comics/new (new functionality) PROTECTED - only signed in users can access
 router.get('/new', ensureSignedIn, (req, res) => {
-  res.send('Add a comic!');
+  res.render('comics/new.ejs', {user: req.user});
+});
+
+// add new comic
+router.post('/', ensureSignedIn, async (req, res) => {
+try {
+  const newComic = await comic.create ({
+    ...req.body,
+    owner: req.user._id,
+  });
+  req.user.comic.push(newComic._id);
+  await req.user.save();
+  res.redirect('/comics');
+} catch (err) {
+  console.error(err);
+  res.redirect('/comics/new');
+}
 });
 
 
