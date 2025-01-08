036c26b00d1ee29894b581620a426c22cf853163
cdenzelcoleman <camdcole@gmail.com>
2025-01-08 00:36:48 -0600

    GET id API Route

--- controllers/api.js
+++ controllers/api.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const { format } = require('morgan');
+const { param } = require('./comics');
 const router = express.Router();
 const API_BASE_URL = 'https://comicvine.gamespot.com/api';
 const API_KEY = process.env.COMIC_VINE_API_KEY || 'c69914b2348593fd0dfeeb4ef47532b1afef5587';
@@ -26,4 +27,27 @@ router.get('comics', async (req,res) => {
             message: 'Failed to fetch comics. Please try again later',
         });
     }
+});
+
+route.get('/comics/:id', async(req, res) => {
+    try{
+        const comicId = req.params.id
+;
+const response = await axios.get(`$API_BASE_URL}/issues/${comicId}`, {
+    param: {
+        api_key: API_KEY,
+        format: 'json',
+    },
+});
+res.json({
+    success: true,
+    data: response.data.results,
+});
+} catch (error) {
+    console.error('Error fetching comics from API:id', error.message);
+    res.status(500).json({
+        success: false,
+        message: 'Failed to fetch comic details. Please try again later.',
+    });
+}
 });
